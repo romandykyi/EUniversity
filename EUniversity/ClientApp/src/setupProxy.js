c8314ed6247bb9687e0c157a1018a4939e086800
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-23 20:30:46 +0200

    Add '/api' to setup proxy context

--- EUniversity/ClientApp/src/setupProxy.js
+++ EUniversity/ClientApp/src/setupProxy.js
@@ -5,6 +5,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:54500';
 
 const context = [
+    "/api",
     "/weatherforecast",
     "/_configuration",
     "/.well-known",
