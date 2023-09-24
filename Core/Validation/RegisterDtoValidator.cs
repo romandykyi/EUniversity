926fd8d2f5273c7fa6be6ce56523ecd9625e68ff
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-24 21:22:49 +0200

    Add empty register validator

---
+++ Core/Validation/RegisterDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using EUniversity.Core.Dtos.Auth;
+using FluentValidation;
+
+namespace EUniversity.Core.Validation
+{
+	public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+	{
+		public RegisterDtoValidator()
+		{
+
+		}
+	}
+}
