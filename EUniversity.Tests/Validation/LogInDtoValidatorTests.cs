24ec32718fd34e2b1ffb377ecc7996abd069d1f1
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-24 21:10:30 +0200

    Add unit tests for login validator

---
+++ EUniversity.Tests/Validation/LogInDtoValidatorTests.cs
@@ -0,0 +1,75 @@
+﻿using EUniversity.Core.Dtos.Auth;
+using EUniversity.Core.Validation;
+
+namespace EUniversity.Tests.Validator
+{
+	public class LogInDtoValidatorTests
+	{
+		private LogInDtoValidator _validator;
+
+		[OneTimeSetUp]
+		public void SetUp()
+		{
+			_validator = new LogInDtoValidator();
+		}
+
+		[Test]
+		public void ValidInput_IsValid()
+		{
+			// Arrange
+			var logInDto = new LogInDto
+			{
+				UserName = "testuser",
+				Password = "password123"
+			};
+
+			// Act
+			var result = _validator.Validate(logInDto);
+
+			// Assert
+			Assert.That(result.IsValid);
+		}
+
+		[Test]
+		public void EmptyUserName_IsInvalid()
+		{
+			// Arrange
+			var logInDto = new LogInDto
+			{
+				UserName = "",
+				Password = "password123"
+			};
+
+			// Act
+			var result = _validator.Validate(logInDto);
+
+			// Assert
+			Assert.Multiple(() =>
+			{
+				Assert.That(result.IsValid, Is.Not.True);
+				Assert.That(result.Errors[0].ErrorMessage, Is.EqualTo("Username is required"));
+			});
+		}
+
+		[Test]
+		public void EmptyPassword_IsInvalid()
+		{
+			// Arrange
+			var logInDto = new LogInDto
+			{
+				UserName = "testuser",
+				Password = ""
+			};
+
+			// Act
+			var result = _validator.Validate(logInDto);
+
+			// Assert
+			Assert.Multiple(() =>
+			{
+				Assert.That(result.IsValid, Is.Not.True);
+				Assert.That(result.Errors[0].ErrorMessage, Is.EqualTo("Password is required"));
+			});
+		}
+	}
+}
