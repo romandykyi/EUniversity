3022062cbb09b0fa1f6151027456c928b44d348a
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-25 20:19:52 +0200

    Implement change password validator

--- Core/Validation/ChangePasswordDtoValidator.cs
+++ Core/Validation/ChangePasswordDtoValidator.cs
@@ -6,7 +6,11 @@ namespace EUniversity.Core.Validation
 	public class ChangePasswordDtoValidator : AbstractValidator<ChangePasswordDto>
 	{
 		public ChangePasswordDtoValidator() 
-		{ 
+		{
+			RuleFor(x => x.Current).NotEmpty().WithMessage("Current password is required");
+			RuleFor(x => x.New).NotEmpty().WithMessage("New password is required");
+
+			RuleFor(x => x.New).NotEqual(x => x.Current).WithMessage("New password cannot be the same as old password");
 		}
 	}
 }
