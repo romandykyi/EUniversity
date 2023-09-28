cb3e3186595cbb2da5d86a8fa28658122c4765e7
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-28 17:21:22 +0200

    Enhance IntegrationTest
    - Added methods for 'clearing' username and email

--- IntegrationTests/IntegrationTest.cs
+++ IntegrationTests/IntegrationTest.cs
@@ -1,5 +1,7 @@
-﻿using EUniversity.Infrastructure.Data;
+﻿using EUniversity.Core.Models;
+using EUniversity.Infrastructure.Data;
 using EUniversity.IntegrationTests.Controllers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EUniversity.IntegrationTests
@@ -17,6 +19,7 @@ namespace EUniversity.IntegrationTests
 		protected IServiceScopeFactory ScopeFactory { get; private set; }
 		protected IServiceScope ServiceScope { get; private set; }
 		protected ApplicationDbContext DbContext { get; private set; }
+		protected UserManager<ApplicationUser> UserManager { get; private set; }
 
 		[SetUp]
 		public void SetUpIntegration()
@@ -28,6 +31,7 @@ namespace EUniversity.IntegrationTests
 			ScopeFactory = WebApplicationFactory.Services.GetService<IServiceScopeFactory>()!;
 			ServiceScope = ScopeFactory.CreateScope();
 			DbContext = ServiceScope.ServiceProvider.GetService<ApplicationDbContext>()!;
+			UserManager = ServiceScope.ServiceProvider.GetService<UserManager<ApplicationUser>>()!;
 
 			// Begin transaction to make tests isolated
 			DbContext.Database.BeginTransaction();
@@ -42,6 +46,32 @@ namespace EUniversity.IntegrationTests
 			ServiceScope.Dispose();
 			Client.Dispose();
 			WebApplicationFactory.Dispose();
+		}
+
+		/// <summary>
+		/// Ensures that username is free.
+		/// </summary>
+		/// <param name="userName">Email that needs to be free.</param>
+		protected async Task ClearUserNameAsync(string userName)
+		{
+			var user = await UserManager.FindByNameAsync(userName);
+			if (user != null)
+			{
+				await UserManager.DeleteAsync(user);
+			}
+		}
+
+		/// <summary>
+		/// Ensures that email is free.
+		/// </summary>
+		/// <param name="userName">Email that needs to be free.</param>
+		protected async Task ClearEmailAsync(string email)
+		{
+			var user = await UserManager.FindByEmailAsync(email);
+			if (user != null)
+			{
+				await UserManager.DeleteAsync(user);
+			}
 		}
 	}
 }
