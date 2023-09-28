3da5b0d0f0d37fc75c020650ea83e52701259eeb
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-28 18:03:06 +0200

    Add tests for mapping global settings

---
+++ EUniversity.Tests/Mapping/MappingGlobalSettingsTests.cs
@@ -0,0 +1,41 @@
+﻿using EUniversity.Core.Mapping;
+using Mapster;
+
+namespace EUniversity.Tests.Maps
+{
+	public class MappingGlobalSettingsTests
+	{
+		private class Poco
+		{
+			public string? NullableString { get; set; }
+		}
+
+		private class Dto
+		{
+			public string? NullableString { get; set; }
+		}
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			MappingGlobalSettings.Apply();
+		}
+
+		[Test]
+		[TestCase("")]
+		[TestCase(" ")]
+		[TestCase("\t")]
+		[TestCase("\r")]
+		public void NullableString_EmptyOrWhiteSpace_MapsToNull(string testCase)
+		{
+			// Arrange
+			Poco poco = new() { NullableString = testCase };
+
+			// Act
+			var dto = poco.Adapt<Dto>();
+
+			// Assert
+			Assert.That(dto.NullableString, Is.Null);
+		}
+	}
+}
