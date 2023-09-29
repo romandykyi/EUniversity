32ec46d78288b546abca0cf9ea842114cbeb4bc5
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-29 21:55:37 +0200

    Add in-memory database support

---
+++ EUniversity/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using EUniversity.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EUniversity.Extensions
+{
+	public static class WebApplicationBuilderExtensions
+	{
+		public static void ConnectDatabase(this WebApplicationBuilder builder)
+		{
+			using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
+						.SetMinimumLevel(LogLevel.Trace)
+						.AddConsole());
+			var logger = loggerFactory.CreateLogger<Program>();
+
+			var useInMemoryDb = Environment.GetEnvironmentVariable("USE_IN_MEMORY_DATABASE");
+			Console.WriteLine(useInMemoryDb);
+			Action<DbContextOptionsBuilder> dbContextOptions;
+			if (useInMemoryDb == null || useInMemoryDb == "false")
+			{
+				// Use SQL Server
+				var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+				dbContextOptions = o => o.UseSqlServer(connectionString, b => b.MigrationsAssembly("EUniversity.Infrastructure"));
+			}
+			else if (useInMemoryDb == "true")
+			{
+				// Use in-memory database
+				dbContextOptions = o => o.UseInMemoryDatabase("EUniversityTestDb");
+
+				logger.LogWarning("In-memory database is used");
+			}
+			else
+			{
+				throw new InvalidOperationException("USE_IN_MEMORY_DATABASE value is invalid");
+			}
+
+			builder.Services.AddDbContext<ApplicationDbContext>(dbContextOptions);
+		}
+	}
+}
