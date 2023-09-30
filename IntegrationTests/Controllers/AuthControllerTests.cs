7e26f0776d446c57949d4a4b0cea6b5a26ccbef0
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-30 21:34:28 +0200

    Add tests for 'api/auth/login'

--- IntegrationTests/Controllers/AuthControllerTests.cs
+++ IntegrationTests/Controllers/AuthControllerTests.cs
@@ -1,23 +1,72 @@
 ﻿using EUniversity.Core.Dtos.Auth;
+using EUniversity.Infrastructure.Services;
+using EUniversity.IntegrationTests.Services;
+using Microsoft.AspNetCore.Identity;
+using NSubstitute;
 using System.Net;
+using System.Net.Http.Headers;
 
 namespace EUniversity.IntegrationTests.Controllers
 {
 	public class AuthControllerTests : ControllersTest
 	{
+		private const string DefaultUserName = "user";
+		private const string DefaultPassword = "Password1!@Gs";
+		private const string NewPassword = DefaultPassword + "2";
+
 		[Test]
-		public async Task LogIn_MalformedData_ReturnsBadRequest()
+		public async Task LogIn_ValidAttempt_Succeeds()
 		{
 			// Arrange
 			using var client = CreateUnauthorizedClient();
-			var malformedLoginDto = new LogInDto()
+			LogInDto loginDto = new()
+			{
+				UserName = DefaultUserName,
+				Password = DefaultPassword
+			};
+			WebApplicationFactory.AuthServiceMock
+				.LogInAsync(Arg.Any<LogInDto>()).Returns(true);
+
+			// Act
+			var response = await client.PostAsJsonAsync("/api/auth/login", loginDto);
+
+			// Assert
+			response.EnsureSuccessStatusCode();
+		}
+
+		[Test]
+		public async Task LogIn_InvalidAttempt_Fails()
+		{
+			// Arrange
+			using var client = CreateUnauthorizedClient();
+			LogInDto loginDto = new()
+			{
+				UserName = DefaultUserName,
+				Password = DefaultPassword
+			};
+			WebApplicationFactory.AuthServiceMock
+				.LogInAsync(loginDto).Returns(false);
+
+			// Act
+			var response = await client.PostAsJsonAsync("/api/auth/login", loginDto);
+
+			// Assert
+			Assert.That(response.IsSuccessStatusCode, Is.False);
+		}
+
+		[Test]
+		public async Task LogIn_InvalidInput_Returns400BadRequest()
+		{
+			// Arrange
+			using var client = CreateUnauthorizedClient();
+			var loginDto = new LogInDto()
 			{
 				UserName = "",
 				Password = ""
 			};
 
 			// Act
-			var response = await client.PostAsJsonAsync("/api/auth/login", malformedLoginDto);
+			var response = await client.PostAsJsonAsync("/api/auth/login", loginDto);
 
 			// Assert
 			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
--- IntegrationTests/Mocks/MockedProgramWebApplicationFactory.cs
+++ IntegrationTests/Mocks/MockedProgramWebApplicationFactory.cs
@@ -1,16 +1,31 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using EUniversity.Core.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 using System.Net.Http.Headers;
 
 namespace EUniversity.IntegrationTests.Mocks
 {
-    /// <summary>
-    /// <see cref="WebApplicationFactory{}" /> for testing with mocked services and authentication.
-    /// </summary>
-    public class MockedProgramWebApplicationFactory : WebApplicationFactory<Program>
+	/// <summary>
+	/// <see cref="WebApplicationFactory{}" /> for testing with mocked services and authentication.
+	/// </summary>
+	public class MockedProgramWebApplicationFactory : WebApplicationFactory<Program>
 	{
+		public IAuthService AuthServiceMock { get; private set; } = null!;
+
+		protected override void ConfigureWebHost(IWebHostBuilder builder)
+		{
+			AuthServiceMock = Substitute.For<IAuthService>();
+
+			builder.ConfigureTestServices(services =>
+			{
+				services.AddScoped(_ => AuthServiceMock);
+			});
+		}
+
 		public WebApplicationFactory<Program> WithAuthentication(TestClaimsProvider claimsProvider)
 		{
 			return WithWebHostBuilder(builder =>
