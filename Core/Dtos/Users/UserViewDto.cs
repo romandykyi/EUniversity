55a3bccdce457af1240b740ad005cf33fddbd361
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-01 20:38:43 +0200

    Add 'UserViewDto.UserName' property

--- Core/Dtos/Users/UserViewDto.cs
+++ Core/Dtos/Users/UserViewDto.cs
@@ -3,6 +3,7 @@
 	public class UserViewDto
 	{
 		public string Email { get; set; } = null!;
+		public string UserName { get; set; } = null!;
 		public string FirstName { get; set; } = null!;
 		public string? MiddleName { get; set; }
 		public string LastName { get; set; } = null!;
