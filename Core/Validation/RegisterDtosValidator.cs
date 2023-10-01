453366bb3ca5a9183eab8a22267c9b1cbaf7ac47
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-01 13:32:36 +0200

    Add validator for IEnumerable<RegisterDto>

---
+++ Core/Validation/RegisterDtosValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using EUniversity.Core.Dtos.Auth;
+
+namespace EUniversity.Core.Validation
+{
+	public class RegisterDtosValidator : AbstractValidator<IEnumerable<RegisterDto>>
+	{
+		public RegisterDtosValidator() 
+		{
+			RuleFor(x => x).NotEmpty()
+				.WithErrorCode(ValidationErrorCodes.EmptyCollection)
+				.WithMessage("Users cannot be empty");
+			RuleForEach(x => x)
+				.SetValidator(new RegisterDtoValidator());
+		}
+	}
+}
