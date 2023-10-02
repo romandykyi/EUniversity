28a40fcba10ded863620ee35f96e872be4174022
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-02 19:56:45 +0200

    Add [ValidateNever] to RegisterUsersDto

--- Core/Dtos/Users/RegisterUsersDto.cs
+++ Core/Dtos/Users/RegisterUsersDto.cs
@@ -1,7 +1,9 @@
 ﻿using EUniversity.Core.Dtos.Auth;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace EUniversity.Core.Dtos.Users
 {
+	[ValidateNever]
 	public class RegisterUsersDto
 	{
 		public IEnumerable<RegisterDto> Users { get; set; } = null!;
