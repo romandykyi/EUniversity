b8b6d811126440bd838d8c565982156036f6d9c3
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-04 23:31:09 +0200

    Fully implement 'UsersController'

--- EUniversity/Controllers/UsersController.cs
+++ EUniversity/Controllers/UsersController.cs
@@ -1,114 +1,129 @@
-﻿using EUniversity.Core.Dtos.Auth;
-using EUniversity.Core.Dtos.Users;
-using EUniversity.Core.Models;
+﻿using EUniversity.Core.Dtos.Users;
 using EUniversity.Core.Policy;
 using EUniversity.Core.Services;
-using EUniversity.Infrastructure.Data;
-using Mapster;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Attributes;
 
 namespace EUniversity.Controllers
 {
-	[ApiController]
-	[Route("api/users")]
-	[FluentValidationAutoValidation]
-	[Authorize(Policies.HasAdministratorPermission)]
-	public class UsersController : ControllerBase
-	{
-		private readonly IAuthService _authService;
-		private readonly IUsersService _usersService;
+    [ApiController]
+    [Route("api/users")]
+    [FluentValidationAutoValidation]
+    [Authorize(Policies.HasAdministratorPermission)]
+    public class UsersController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        private readonly IUsersService _usersService;
 
-		public UsersController(IAuthService authService, IUsersService usersService)
-		{
-			_authService = authService;
-			_usersService = usersService;
-		}
+        public UsersController(IAuthService authService, IUsersService usersService)
+        {
+            _authService = authService;
+            _usersService = usersService;
+        }
 
-		#region Get
-		/// <summary>
-		/// Gets all users.
-		/// </summary>
-		/// <response code="200">Returns all users</response>
-		/// <response code="401">Unauthorized user call</response>
-		/// <response code="403">User lacks 'Administrator' role</response>
-		[HttpGet]
-		[ProducesResponseType(typeof(UsersViewDto), StatusCodes.Status200OK)]
-		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
-		[ProducesResponseType(StatusCodes.Status403Forbidden)]
-		public async Task<IActionResult> GetAllUsersAsync()
-		{
-			return Ok(await _usersService.GetAllUsersAsync());
-		}
+        #region Get
+        /// <summary>
+        /// Gets all users.
+        /// </summary>
+        /// <response code="200">Returns all users</response>
+        /// <response code="401">Unauthorized user call</response>
+        /// <response code="403">User lacks 'Administrator' role</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(UsersViewDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<IActionResult> GetAllUsersAsync()
+        {
+            return Ok(await _usersService.GetAllUsersAsync());
+        }
 
-		/// <summary>
-		/// Gets all students.
-		/// </summary>
-		/// <response code="200">Returns all students</response>
-		/// <response code="401">Unauthorized user call</response>
-		/// <response code="403">User lacks 'Administrator' role</response>
-		[HttpGet]
-		[Route("students")]
-		[ProducesResponseType(typeof(UsersViewDto), StatusCodes.Status200OK)]
-		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
-		[ProducesResponseType(StatusCodes.Status403Forbidden)]
-		public async Task<IActionResult> GetAllStudentsAsync()
-		{
-			return Ok(await _usersService.GetUsersInRoleAsync(Roles.Student));
-		}
+        /// <summary>
+        /// Gets all students.
+        /// </summary>
+        /// <response code="200">Returns all students</response>
+        /// <response code="401">Unauthorized user call</response>
+        /// <response code="403">User lacks 'Administrator' role</response>
+        [HttpGet]
+        [Route("students")]
+        [ProducesResponseType(typeof(UsersViewDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<IActionResult> GetAllStudentsAsync()
+        {
+            return Ok(await _usersService.GetUsersInRoleAsync(Roles.Student));
+        }
 
-		/// <summary>
-		/// Gets all teachers.
-		/// </summary>
-		/// <response code="200">Returns all teachers</response>
-		/// <response code="401">Unauthorized user call</response>
-		/// <response code="403">User lacks 'Administrator' role</response>
-		[HttpGet]
-		[Route("teachers")]
-		[ProducesResponseType(typeof(UsersViewDto), StatusCodes.Status200OK)]
-		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
-		[ProducesResponseType(StatusCodes.Status403Forbidden)]
-		public async Task<IActionResult> GetAllTeachersAsync()
+        /// <summary>
+        /// Gets all teachers.
+        /// </summary>
+        /// <response code="200">Returns all teachers</response>
+        /// <response code="401">Unauthorized user call</response>
+        /// <response code="403">User lacks 'Administrator' role</response>
+        [HttpGet]
+        [Route("teachers")]
+        [ProducesResponseType(typeof(UsersViewDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<IActionResult> GetAllTeachersAsync()
         {
             return Ok(await _usersService.GetUsersInRoleAsync(Roles.Teacher));
         }
-		#endregion
+        #endregion
 
-		#region Post
-		/// <summary>
-		/// Registers students.
-		/// </summary>
-		/// <response code="201">Returns registered students</response>
-		/// <response code="401">Unauthorized user call</response>
-		/// <response code="403">User lacks 'Administrator' role</response>
-		[HttpPost]
-		[Route("students")]
-		[ProducesResponseType(typeof(IEnumerable<CreatedUserDto>), StatusCodes.Status201Created)]
-		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
-		[ProducesResponseType(StatusCodes.Status403Forbidden)]
-		public async Task<IActionResult> RegisterStudentsAsync([FromBody] RegisterUsersDto students)
-		{
-			return StatusCode(StatusCodes.Status503ServiceUnavailable);
-		}
+        #region Post
+        private async Task<IActionResult> RegisterAsync(RegisterUsersDto students, string role, string location)
+        {
+            List<CreatedUserDto> createdUsers = new();
+            await foreach (var result in _authService.RegisterManyAsync(students.Users, role))
+            {
+                CreatedUserDto createdUser = new(
+                    result.UserName,
+                    result.Password,
+                    result.Succeeded,
+                    result.Succeeded ? null : result.Errors.Select(e => e.Description)
+                );
+                createdUsers.Add(createdUser);
+            }
 
-		/// <summary>
-		/// Registers teachers.
-		/// </summary>
-		/// <response code="201">Returns registered teachers</response>
-		/// <response code="401">Unauthorized user call</response>
-		/// <response code="403">User lacks 'Administrator' role</response>
-		[HttpPost]
-		[Route("teachers")]
-		[ProducesResponseType(typeof(IEnumerable<CreatedUserDto>), StatusCodes.Status201Created)]
-		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
-		[ProducesResponseType(StatusCodes.Status403Forbidden)]
-		public async Task<IActionResult> RegisterTeachersAsync([FromBody] RegisterUsersDto teachers)
-		{
-			return StatusCode(StatusCodes.Status503ServiceUnavailable);
-		}
-		#endregion
-	}
+            return Created(location, createdUsers);
+        }
+
+        /// <summary>
+        /// Registers students.
+        /// </summary>
+        /// <response code="201">Returns status of students</response>
+        /// <response code="400">Invalid input</response>
+        /// <response code="401">Unauthorized user call</response>
+        /// <response code="403">User lacks 'Administrator' role</response>
+        [HttpPost]
+        [Route("students")]
+        [ProducesResponseType(typeof(IEnumerable<CreatedUserDto>), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<IActionResult> RegisterStudentsAsync([FromBody] RegisterUsersDto students)
+        {
+            return await RegisterAsync(students, Roles.Student, "api/users/students");
+        }
+
+        /// <summary>
+        /// Registers teachers.
+        /// </summary>
+        /// <response code="201">Returns status of teachers</response>
+        /// <response code="400">Invalid input</response>
+        /// <response code="401">Unauthorized user call</response>
+        /// <response code="403">User lacks 'Administrator' role</response>
+        [HttpPost]
+        [Route("teachers")]
+        [ProducesResponseType(typeof(IEnumerable<CreatedUserDto>), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<IActionResult> RegisterTeachersAsync([FromBody] RegisterUsersDto teachers)
+        {
+            return await RegisterAsync(teachers, Roles.Teacher, "api/users/teachers");
+        }
+        #endregion
+    }
 }
