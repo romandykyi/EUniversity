4199c84e1e3a982a328ce91664f0ede8236afdcc
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-05 22:46:31 +0200

    Add 'ApplyPagination' exteinsion for 'IQueryable<T>'

---
+++ Core/Pagination/IQueryablePaginationExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace EUniversity.Core.Pagination
+{
+    public static class QueryablePaginationExtensions
+    {
+        public static IQueryable<T> ApplyPagination<T>(
+            this IQueryable<T> queryable, PaginationProperties properties)
+        {
+            return queryable
+                .Skip((properties.Page - 1) * properties.PageSize)
+                .Take(properties.PageSize);
+        }
+    }
+}
