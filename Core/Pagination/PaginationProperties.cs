9ab008e5b9ee11af5a086c89d6c174ccd35ac073
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-05 22:27:26 +0200

    Add 'PaginationProperties'

---
+++ Core/Pagination/PaginationProperties.cs
@@ -0,0 +1,8 @@
+﻿namespace EUniversity.Core.Pagination
+{
+    public record struct PaginationProperties(int Page, int PageSize)
+    {
+        public const int MinPageSize = 5;
+        public const int MaxPageSize = 100;
+    }
+}
