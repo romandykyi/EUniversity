cca10b8060f66be0e55a594058458f5bdfe8f765
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-05 21:40:02 +0200

    Add 'CreateClassrromDto'
    - Added DTO for creating Classroom entity
    - Added validator for this DTO with unit tests

---
+++ Core/Validation/CreateClassroomDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using EUniversity.Core.Dtos.University;
+using EUniversity.Core.Models;
+using FluentValidation;
+
+namespace EUniversity.Core.Validation
+{
+    public class CreateClassroomDtoValidator : AbstractValidator<CreateClassromDto>
+    {
+        public CreateClassroomDtoValidator() 
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .WithErrorCode(ValidationErrorCodes.PropertyRequired)
+                .WithMessage("Classroom name is required");
+
+            RuleFor(c => c.Name)
+                .MaximumLength(Classroom.MaxNameLength)
+                .WithErrorCode(ValidationErrorCodes.PropertyTooLarge)
+                .WithMessage($"Classroom name cannot exceed {Classroom.MaxNameLength} characters");
+        }
+    }
+}
