630130f360bb86979302f25ac424348ba1f761f2
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-09 00:50:44 +0200

    Add base CRUD controllers integration tests

---
+++ IntegrationTests/Controllers/University/ClassroomsControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using EUniversity.Core.Dtos.University;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace EUniversity.IntegrationTests.Controllers.University
+{
+    public class ClassroomsControllerTests : ControllersTest
+    {
+        public static readonly CreateClassromDto ValidCreateClassroomDto = new("300");
+
+        public const string GetByIdRoute = "/api/classrooms/{0}";
+        public const string CreateRoute = "/api/classrooms";
+        public const string UpdateRoute = "/api/classrooms/{0}";
+        public const string DeleteRoute = "/api/classrooms/{0}";
+
+    }
+}
