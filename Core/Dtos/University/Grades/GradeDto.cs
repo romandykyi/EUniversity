4fa7f5f545aa6adc36bf5c64fe972aa43688d10a
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-18 11:37:28 +0200

    Add GradeDto
    Changes made:
    * Added record GradeDto
    * Implemented validator for this record
    * Wrote unit tests for this validator

---
+++ Core/Dtos/University/Grades/GradeDto.cs
@@ -0,0 +1,4 @@
+﻿namespace EUniversity.Core.Dtos.University.Grades
+{
+    public record GradeDto(string Name, int Score);
+}
---
+++ Core/Validation/University/Grades/GradeDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using EUniversity.Core.Dtos.University.Grades;
+using EUniversity.Core.Models.University;
+using EUniversity.Core.Models.University.Grades;
+using FluentValidation;
+
+namespace EUniversity.Core.Validation.University.Grades
+{
+    public class GradeDtoValidator : AbstractValidator<GradeDto>
+    {
+        public GradeDtoValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .WithErrorCode(ValidationErrorCodes.PropertyRequired)
+                .WithMessage("Grade name is required");
+
+            RuleFor(c => c.Name)
+                .MaximumLength(Grade.MaxNameLength)
+                .WithErrorCode(ValidationErrorCodes.PropertyTooLarge)
+                .WithMessage($"Grade name cannot exceed {Grade.MaxNameLength} characters");
+        }
+    }
+}
