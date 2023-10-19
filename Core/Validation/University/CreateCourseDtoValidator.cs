1ce630eee59f6d1420e9ce7af33c58982896adcb
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-19 13:47:52 +0200

    Add tests for course creation validation

---
+++ Core/Validation/University/CreateCourseDtoValidator.cs
@@ -0,0 +1,12 @@
+﻿using EUniversity.Core.Dtos.University;
+using FluentValidation;
+
+namespace EUniversity.Core.Validation.University
+{
+    public class CreateCourseDtoValidator : AbstractValidator<CreateCourseDto>
+    {
+        public CreateCourseDtoValidator() 
+        { 
+        }
+    }
+}
