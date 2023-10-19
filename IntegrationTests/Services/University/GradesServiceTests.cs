7f0bc44a4da47f861511cb7e1788d4764a3618b8
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-19 12:37:11 +0200

    Write tests for GradesService

---
+++ IntegrationTests/Services/University/GradesServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using EUniversity.Core.Dtos.University;
+using EUniversity.Core.Dtos.University.Grades;
+using EUniversity.Core.Models.University;
+using EUniversity.Core.Models.University.Grades;
+using EUniversity.Infrastructure.Services.University;
+
+namespace EUniversity.IntegrationTests.Services.University
+{
+    public class GradesServiceTests : CrudServicesTest
+        <IGradesService, Grade, int, GradeDto, GradeDto, GradeDto, GradeDto>
+    {
+        /// <inheritdoc />
+        protected override void AssertThatWasUpdated(Grade actualEntity, GradeDto updateDto)
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(actualEntity.Name, Is.EqualTo(updateDto.Name));
+                Assert.That(actualEntity.Score, Is.EqualTo(updateDto.Score));
+            });
+        }
+
+        /// <inheritdoc />
+        protected override int GetNonExistentId() => -1;
+
+        /// <inheritdoc />
+        protected override Grade GetTestEntity()
+        {
+            return new()
+            {
+                Name = "3",
+                Score = 3
+            };
+        }
+
+        /// <inheritdoc />
+        protected override GradeDto GetValidCreateDto()
+        {
+            return new("5", 5);
+        }
+
+        /// <inheritdoc />
+        protected override GradeDto GetValidUpdateDto()
+        {
+            return new("4", 4);
+        }
+    }
+}
