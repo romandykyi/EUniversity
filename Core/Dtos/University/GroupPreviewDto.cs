d0b51ffb56b14d85ed47bb14b8231ce7cdca3b60
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-27 16:57:21 +0200

    Add DTOs for groups

---
+++ Core/Dtos/University/GroupPreviewDto.cs
@@ -0,0 +1,5 @@
+﻿using EUniversity.Core.Dtos.Users;
+
+namespace EUniversity.Core.Dtos.University;
+
+public record GroupPreviewDto(int Id, string Name, TeacherPreviewDto Teacher, CoursePreviewDto Course);
