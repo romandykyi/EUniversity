21bb17dfa296b97d5bc534a1aba931d3dd9988fd
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-27 22:13:35 +0200

    Add `IGroupsService` and tests for it

--- IntegrationTests/Services/ServicesTest.cs
+++ IntegrationTests/Services/ServicesTest.cs
@@ -1,4 +1,6 @@
-﻿using EUniversity.Infrastructure.Data;
+﻿using EUniversity.Core.Models;
+using EUniversity.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EUniversity.IntegrationTests.Services;
@@ -27,5 +29,28 @@ public abstract class ServicesTest : IntegrationTest<ProgramWebApplicationFactor
         // Undo all changes
         DbContext.ChangeTracker.Clear();
         DbContext.Database.RollbackTransaction();
+    }
+
+    /// <summary>
+    /// Registers a test user.
+    /// </summary>
+    /// <param name="roles">Roles that will be assigned to the user.</param>
+    /// <returns>
+    /// <see cref="ApplicationUser"/> that exists in the test database.
+    /// </returns>
+    protected async Task<ApplicationUser> RegisterTestUser(params string[] roles)
+    {
+        ApplicationUser user = new()
+        {
+            UserName = "test-user",
+            FirstName = "Test1",
+            LastName = "Test2",
+            Email = "test@example.com"
+        };
+        var userManager = ServiceScope.ServiceProvider.GetService<UserManager<ApplicationUser>>()!;
+        await userManager.CreateAsync(user, "StrongPa$$w0rd");
+        await userManager.AddToRolesAsync(user, roles);
+
+        return user;
     }
 }
---
+++ IntegrationTests/Services/University/GroupsServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using EUniversity.Core.Dtos.University;
+using EUniversity.Core.Models;
+using EUniversity.Core.Models.University;
+using EUniversity.Core.Policy;
+using EUniversity.Core.Services.University;
+
+namespace EUniversity.IntegrationTests.Services.University;
+
+public class GroupsServiceTests :
+    CrudServicesTest<IGroupsService, Group, int, GroupPreviewDto, GroupViewDto, GroupCreateDto, GroupCreateDto>
+{
+    private Course _testCourse;
+    private ApplicationUser _testTeacher;
+
+    /// <inheritdoc />
+    protected override void AssertThatWasUpdated(Group actualEntity, GroupCreateDto updateDto)
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(actualEntity.Name, Is.EqualTo(updateDto.Name));
+            Assert.That(actualEntity.TeacherId, Is.EqualTo(updateDto.TeacherId));
+            Assert.That(actualEntity.CourseId, Is.EqualTo(updateDto.CourseId));
+        });
+    }
+
+    /// <inheritdoc />
+    protected override int GetNonExistentId()
+    {
+        return -1;
+    }
+
+    /// <inheritdoc />
+    protected override Group GetTestEntity()
+    {
+        return new Group()
+        {
+            Name = "100-A",
+            CourseId = _testCourse.Id,
+            TeacherId = _testTeacher.Id
+        };
+    }
+
+    /// <inheritdoc />
+    protected override GroupCreateDto GetValidCreateDto()
+    {
+        return new("112-A", _testCourse.Id, _testTeacher.Id);
+    }
+
+    /// <inheritdoc />
+    protected override GroupCreateDto GetValidUpdateDto()
+    {
+        return new("100-B", _testCourse.Id, null);
+    }
+
+    [SetUp]
+    public async Task SetUpDependencies()
+    {
+        _testCourse = CoursesServiceTests.CreateTestCourse();
+        DbContext.Add(_testCourse);
+        await DbContext.SaveChangesAsync();
+
+        _testTeacher = await RegisterTestUser(Roles.Teacher);
+    }
+}
