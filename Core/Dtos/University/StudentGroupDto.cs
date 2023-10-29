fe54bc38195ca7c5f92548f458cf6942e76fd7a3
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-29 21:18:16 +0100

    Add [ValidateNever] to `StudentGroupDto`

--- Core/Dtos/University/StudentGroupDto.cs
+++ Core/Dtos/University/StudentGroupDto.cs
@@ -1,3 +1,6 @@
-﻿namespace EUniversity.Core.Dtos.University;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
+namespace EUniversity.Core.Dtos.University;
+
+[ValidateNever] // Remove data annotations validation
 public record StudentGroupDto(string StudentId, int GroupId);
