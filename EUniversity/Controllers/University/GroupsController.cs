ca0ecbf07cb9096f0694d43d8cb0d38b4b92ce17
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-30 22:15:18 +0100

    Implement student groups controller methods

--- EUniversity/Controllers/University/GroupsController.cs
+++ EUniversity/Controllers/University/GroupsController.cs
@@ -1,10 +1,14 @@
 ﻿using EUniversity.Core.Dtos.University;
+using EUniversity.Core.Models;
+using EUniversity.Core.Models.University;
 using EUniversity.Core.Pagination;
 using EUniversity.Core.Policy;
+using EUniversity.Core.Services;
 using EUniversity.Core.Services.University;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Attributes;
+using System.Diagnostics;
 
 namespace EUniversity.Controllers.University;
 
@@ -17,10 +21,13 @@ namespace EUniversity.Controllers.University;
 public class GroupsController : ControllerBase
 {
     private readonly IGroupsService _groupsService;
+    private readonly IEntityExistenceChecker _existenceChecker;
 
-    public GroupsController(IGroupsService groupsService)
+    public GroupsController(IGroupsService groupsService,
+        IEntityExistenceChecker existenceChecker)
     {
         _groupsService = groupsService;
+        _existenceChecker = existenceChecker;
     }
 
     /// <summary>
@@ -123,5 +130,89 @@ public class GroupsController : ControllerBase
     {
         var result = await _groupsService.DeleteAsync(id);
         return result ? NoContent() : NotFound();
+    }
+
+    private IActionResult CustomNotFound(string message)
+    {
+        return NotFound(new
+        {
+            type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            title = "Resource not found",
+            status = StatusCodes.Status404NotFound,
+            traceId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
+            message
+        });
+    }
+
+    /// <summary>
+    /// Adds a student to a group.
+    /// </summary>
+    /// <response code="200">Success(user is already part of the group)</response>
+    /// <response code="201">Success(user was added to the group)</response>
+    /// <response code="400">Malformed input, or invalid user ID, or user is not a student</response>
+    /// <response code="401">Unauthorized user call</response>
+    /// <response code="403">User lacks 'Administrator' role</response>
+    /// <response code="404">Group does not exist</response>
+    [HttpPost]
+    [Route("{groupId:int}/students")]
+    [Authorize(Policies.HasAdministratorPermission)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> AddStudentToGroupAsync(
+        [FromRoute] int groupId, [FromBody] StudentGroupCreateDto dto)
+    {
+        if (!await _existenceChecker.ExistsAsync<Group, int>(groupId))
+        {
+            return CustomNotFound("The group with the specified ID does not exist.");
+        }
+        bool created = await _groupsService.AddStudentAsync(dto.StudentId, groupId);
+
+        if (created)
+        {
+            var routeValues = new { groupId, studentId = dto.StudentId };
+            return CreatedAtRoute(nameof(DeleteStudentFromGroupAsync), routeValues, null);
+        }
+        return Ok(new { message = "The student is already part of the group" });
+
+    }
+
+    /// <summary>
+    /// Deletes a student from a group.
+    /// </summary>
+    /// <response code="204">Success(user was removed from the group)</response>
+    /// <response code="400">Malformed/invalid input</response>
+    /// <response code="401">Unauthorized user call</response>
+    /// <response code="403">User lacks 'Administrator' role</response>
+    /// <response code="404">Group/Student does not exist or student is not part of the group</response>
+    [HttpDelete]
+    [Route("{groupId:int}/students/{studentId}")]
+    [Authorize(Policies.HasAdministratorPermission)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteStudentFromGroupAsync(
+        [FromRoute] int groupId, [FromRoute] string studentId)
+    {
+        if (!await _existenceChecker.ExistsAsync<Group, int>(groupId))
+        {
+            return CustomNotFound("The group with the specified ID does not exist.");
+        }
+        if (!await _existenceChecker.ExistsAsync<ApplicationUser, string>(studentId))
+        {
+            return CustomNotFound("The user with the specified ID does not exist.");
+        }
+
+        bool removed = await _groupsService.RemoveStudentAsync(studentId, groupId);
+        if (removed)
+        {
+            return NoContent();
+        }
+        return CustomNotFound("The user is not part of the group.");
     }
 }
