7b876596eba22d514aae9a7361c929ce24602afc
romandykyi <roman.dykyi.8210@gmail.com>
2023-11-15 12:08:46 +0100

    Add `UsersFilter` and `UsersFilterProperties`

---
+++ Core/Filters/UsersFilter.cs
@@ -0,0 +1,32 @@
+﻿using EUniversity.Core.Models;
+
+namespace EUniversity.Core.Filters;
+
+/// <summary>
+/// Filter for users.
+/// </summary>
+public class UsersFilter : IFilter<ApplicationUser>
+{
+    public UsersFilterProperties Properties { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the class with the specified properties.
+    /// </summary>
+    /// <param name="properties">Properties of filtering.</param>
+    public UsersFilter(UsersFilterProperties properties)
+    {
+        Properties = properties;
+    }
+
+    /// <summary>
+    /// Apply the users filter to a query.
+    /// </summary>
+    /// <param name="query">The query that needs to be filtered.</param>
+    /// <returns>
+    /// Filtered query that contains entities which satisfy filter properties.
+    /// </returns>
+    public IQueryable<ApplicationUser> Apply(IQueryable<ApplicationUser> query)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Core/Filters/UsersFilterProperties.cs
@@ -0,0 +1,9 @@
+﻿namespace EUniversity.Core.Filters;
+
+/// <summary>
+/// Represents properties for filtering users.
+/// </summary>
+/// <param name="FullName">Optional full name to filter by.</param>
+/// <param name="UserName">Optional username to filter by.</param>
+/// <param name="Email">Optional email to filter by.</param>
+public record UsersFilterProperties(string? FullName, string? UserName, string? Email);
