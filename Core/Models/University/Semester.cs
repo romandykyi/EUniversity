c9b7831cfd20a90d09958195634083a0f7367614
romandykyi <roman.dykyi.8210@gmail.com>
2023-11-18 14:01:40 +0100

    Add `Semester` and `StudentSemester` entities

---
+++ Core/Models/University/Semester.cs
@@ -0,0 +1,24 @@
+﻿namespace EUniversity.Core.Models.University;
+
+public class Semester : IEntity<int>
+{
+    [Key]
+    public int Id { get; set; }
+    /// <summary>
+    /// Date when the semester starts.
+    /// </summary>
+    public DateTimeOffset DateFrom { get; set; }
+    /// <summary>
+    /// Date when the semester ends.
+    /// </summary>
+    public DateTimeOffset DateTo { get; set; }
+
+    /// <summary>
+    /// Navigation property to the students which are part of this semester.
+    /// </summary>
+    public ICollection<ApplicationUser> Students { get; set; } = null!;
+    /// <summary>
+    /// Navigation property to the student enrollments of this semester.
+    /// </summary>
+    public ICollection<StudentSemester> StudentEnrollments { get; set; } = null!;
+}
---
+++ Core/Models/University/StudentSemester.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EUniversity.Core.Models.University;
+
+/// <summary>
+/// Represents the entity used for configuring many-to-many relationship
+/// between students and semesters.
+/// </summary>
+public class StudentSemester : IEntity<int>
+{
+    [Key]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Date when student was added to the semester.
+    /// </summary>
+    public DateTimeOffset EnrolmentDate { get; set; }
+
+    /// <summary>
+    /// Foreign key of the associated student.
+    /// </summary>
+    [ForeignKey(nameof(Student))]
+    public string StudentId { get; set; } = null!;
+    /// <summary>
+    /// Foreign key of the associated semester.
+    /// </summary>
+    [ForeignKey(nameof(Semester))]
+    public int SemesterId { get; set; }
+
+    /// <summary>
+    /// Navigation property for the student associated with this semester.
+    /// </summary>
+    public ApplicationUser Student { get; set; } = null!;
+    /// <summary>
+    /// Navigation property for the semester that the student is part of.
+    /// </summary>
+    public Semester Semester { get; set; } = null!;
+}
