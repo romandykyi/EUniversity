2f791c1f4637efebd025a43d2aac0a12b86fd3d8
romandykyi <roman.dykyi.8210@gmail.com>
2023-11-19 19:59:38 +0100

    Fix enrollments display

--- Infrastructure/Services/University/SemestersService.cs
+++ Infrastructure/Services/University/SemestersService.cs
@@ -9,6 +9,12 @@ public class SemestersService :
     BaseCrudService<Semester, int, SemesterPreviewDto, SemesterViewDto, SemesterCreateDto, SemesterCreateDto>,
     ISemestersService
 {
+    protected override IQueryable<Semester> GetByIdQuery =>
+        Entities
+        .Include(e => e.StudentEnrollments)
+        .ThenInclude(e => e.Student)
+        .AsNoTracking();
+
     public SemestersService(ApplicationDbContext dbContext) : base(dbContext)
     {
     }
