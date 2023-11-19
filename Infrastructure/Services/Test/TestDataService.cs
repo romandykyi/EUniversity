3d22913bbabcb5b3b8ac5a59cba92a3d32427658
romandykyi <roman.dykyi.8210@gmail.com>
2023-11-19 21:28:40 +0100

    Enhance fake data generation
    Now enrollment date for `StudentSemester` is generated randomly

--- Infrastructure/Services/Test/TestDataService.cs
+++ Infrastructure/Services/Test/TestDataService.cs
@@ -272,7 +272,8 @@ public class TestDataService
             int i = 0;
             var studentSemesterFaker = new Faker<StudentSemester>()
                 .RuleFor(s => s.SemesterId, _ => semesterId)
-                .RuleFor(s => s.StudentId, _ => shuffledStudentsIds[i++]);
+                .RuleFor(s => s.StudentId, _ => shuffledStudentsIds[i++])
+                .RuleFor(s => s.EnrollmentDate, f => f.Date.Recent(180));
             int studentsCount = faker.Random.Int(minStudentsInSemester, maxStudentsInSemester);
             await CreateFakeEntitiesAsync(studentSemesterFaker, studentsCount, false);
         }
