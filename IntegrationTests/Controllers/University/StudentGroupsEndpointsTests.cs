7ddd250c3e93e397b54225536c42354bcd519886
romandykyi <roman.dykyi.8210@gmail.com>
2023-11-26 16:17:27 +0100

    Extend integration tests for assigning endpoints

--- IntegrationTests/Controllers/University/StudentGroupsEndpointsTests.cs
+++ IntegrationTests/Controllers/University/StudentGroupsEndpointsTests.cs
@@ -1,4 +1,6 @@
 ﻿using EUniversity.Core.Dtos.University;
+using EUniversity.Core.Dtos.Users;
+using EUniversity.Core.Filters;
 using EUniversity.Core.Models;
 using EUniversity.Core.Models.University;
 using EUniversity.Core.Services.University;
@@ -6,11 +8,13 @@ using EUniversity.Core.Services.University;
 namespace EUniversity.IntegrationTests.Controllers.University;
 
 public class StudentGroupsEndpointsTests :
-    AssigningEndpointsTests<IStudentGroupsService, StudentGroup, Group, int, ApplicationUser, string>
+    AssigningEndpointsTests<IStudentGroupsService, StudentGroup, Group, int, ApplicationUser, string, StudentGroupViewDto>
 {
     public override int TestId1 => 5;
 
     public override string TestId2 => "test-user-id";
+
+    public override string GetPageRoute => $"api/groups/{TestId1}/students";
 
     public override string AssignRoute => $"api/groups/{TestId1}/students";
 
@@ -25,5 +29,11 @@ public class StudentGroupsEndpointsTests :
     protected override object GetAssignDto()
     {
         return new AssignStudentDto(TestId2);
+    }
+
+    protected override StudentGroupViewDto GetTestPreviewDto()
+    {
+        StudentPreviewDto student = new("test-id", "test-user", "Test", "User", null);
+        return new(student, DateTimeOffset.Now);
     }
 }
