e0ea3996b7c175ca37080e2b59ba817526b12345
romandykyi <roman.dykyi.8210@gmail.com>
2023-11-29 11:12:21 +0100

    Add tests for `StudentEnrollmentsFilter`

---
+++ Infrastructure/Filters/StudentEnrollmentsFilter.cs
@@ -0,0 +1,26 @@
+﻿using EUniversity.Core.Filters;
+using EUniversity.Core.Models;
+
+namespace EUniversity.Infrastructure.Filters;
+
+/// <summary>
+/// Filter for student enrollments.
+/// </summary>
+public class StudentEnrollmentsFilter : IFilter<IStudentEnrollment>
+{
+    public StudentEnrollmentsFilterProperties Properties { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the class with the specified properties.
+    /// </summary>
+    /// <param name="properties">Properties of filtering.</param>
+    public StudentEnrollmentsFilter(StudentEnrollmentsFilterProperties properties)
+    {
+        Properties = properties;
+    }
+
+    public IQueryable<IStudentEnrollment> Apply(IQueryable<IStudentEnrollment> query)
+    {
+        throw new NotImplementedException();
+    }
+}
