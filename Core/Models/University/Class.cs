a8f81587393f6e3ab02a2d5504a592710653c6cc
romandykyi <roman.dykyi.8210@gmail.com>
2023-12-02 17:08:56 +0100

    Add `Class` entity

---
+++ Core/Models/University/Class.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EUniversity.Core.Models.University;
+
+/// <summary>
+/// Represents a class schedule entity, 
+/// which contains date, duration, group and optional substitute teacher.
+/// </summary>
+public class Class : IEntity<int>, IHasCreationDate, IHasUpdateDate
+{
+    [Key]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Foreign key of the classroom associated with this class.
+    /// </summary>
+    [ForeignKey(nameof(Classroom))]
+    public int ClassroomId { get; set; }
+    /// <summary>
+    /// Foreign key of the group associated with this class.
+    /// </summary>
+    [ForeignKey(nameof(Group))]
+    public int GroupId { get; set; }
+    /// <summary>
+    /// Foreign key of the substitute teacher associated with this class(can be null).
+    /// </summary>
+    [ForeignKey(nameof(SubstituteTeacher))]
+    public string? SubstituteTeacherId { get; set; }
+
+    /// <summary>
+    /// Date when class starts.
+    /// </summary>
+    public DateTimeOffset StartDate { get; set; }
+
+    /// <summary>
+    /// Duration of this class in ticks.
+    /// </summary>
+    public long DurationTicks { get; set; }
+    /// <summary>
+    /// Duration of this class.
+    /// </summary>
+    public TimeSpan Duration
+    {
+        get => new(DurationTicks);
+        set => DurationTicks = value.Ticks;
+    }
+
+    /// <summary>
+    /// Date when the class was created.
+    /// </summary>
+    public DateTimeOffset CreationDate { get; set; }
+    /// <summary>
+    /// Date when the class was last updated.
+    /// </summary>
+    public DateTimeOffset UpdateDate { get; set; }
+
+    /// <summary>
+    /// Navigation property for the classroom associated with this class.
+    /// </summary>
+    public Classroom? Classroom { get; set; }
+    /// <summary>
+    /// Navigation property for the group associated with this class.
+    /// </summary>
+    public Group? Group { get; set; }
+    /// <summary>
+    /// Navigation property for the substitute associated with this class(can be null).
+    /// </summary>
+    public ApplicationUser? SubstituteTeacher { get; set; }
+}
---
+++ Infrastructure/Migrations/20231202160414_AddClasses.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EUniversity.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class AddClasses : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "Classes",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                ClassroomId = table.Column<int>(type: "int", nullable: false),
+                GroupId = table.Column<int>(type: "int", nullable: false),
+                SubstituteTeacherId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                StartDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                DurationTicks = table.Column<long>(type: "bigint", nullable: false),
+                Duration = table.Column<TimeSpan>(type: "time", nullable: false),
+                CreationDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                UpdateDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Classes", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_Classes_AspNetUsers_SubstituteTeacherId",
+                    column: x => x.SubstituteTeacherId,
+                    principalTable: "AspNetUsers",
+                    principalColumn: "Id");
+                table.ForeignKey(
+                    name: "FK_Classes_Classrooms_ClassroomId",
+                    column: x => x.ClassroomId,
+                    principalTable: "Classrooms",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_Classes_Groups_GroupId",
+                    column: x => x.GroupId,
+                    principalTable: "Groups",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateIndex(
+            name: "IX_Classes_ClassroomId",
+            table: "Classes",
+            column: "ClassroomId");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_Classes_GroupId",
+            table: "Classes",
+            column: "GroupId");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_Classes_SubstituteTeacherId",
+            table: "Classes",
+            column: "SubstituteTeacherId");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Classes");
+    }
+}
