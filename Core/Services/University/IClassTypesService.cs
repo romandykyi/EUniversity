77f4b3a2ac35fe2ea979c0125f9b3dc9aa530704
romandykyi <roman.dykyi.8210@gmail.com>
2023-12-05 17:28:48 +0100

    Implement class types service

---
+++ Core/Services/University/IClassTypesService.cs
@@ -0,0 +1,9 @@
+﻿using EUniversity.Core.Dtos.University;
+using EUniversity.Core.Models.University;
+
+namespace EUniversity.Core.Services.University;
+
+public interface IClassTypesService :
+    ICrudService<ClassType, int, ClassTypeViewDto, ClassTypeViewDto, ClassTypeCreateDto, ClassTypeCreateDto>
+{
+}
---
+++ Infrastructure/Services/University/ClassTypesService.cs
@@ -0,0 +1,14 @@
+﻿using EUniversity.Core.Dtos.University;
+using EUniversity.Core.Models.University;
+using EUniversity.Core.Services.University;
+using EUniversity.Infrastructure.Data;
+
+namespace EUniversity.Infrastructure.Services.University;
+
+/// <inheritdoc />
+public class ClassTypesService :
+    BaseCrudService<ClassType, int, ClassTypeViewDto, ClassTypeViewDto, ClassTypeCreateDto, ClassTypeCreateDto>,
+    IClassTypesService
+{
+    public ClassTypesService(ApplicationDbContext dbContext) : base(dbContext) { }
+}
