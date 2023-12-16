40bea1148f26482234e49b2df17e62cb04b58240
romandykyi <roman.dykyi.8210@gmail.com>
2023-12-16 17:31:17 +0100

    Implement courses filter

---
+++ EUniversity.Tests/Filters/CoursesFilterTests.cs
@@ -0,0 +1,74 @@
+﻿using EUniversity.Core.Models.University;
+using EUniversity.Infrastructure.Filters;
+
+namespace EUniversity.Tests.Filters;
+
+public class CoursesFilterTests
+{
+    private static readonly Semester TestSemester = new()
+    {
+        Id = 100
+    };
+
+    private static readonly Course[] TestCourses =
+    {
+        new()
+        {
+            Id = 200,
+            Name = "Math",
+            Semester = TestSemester,
+            SemesterId = TestSemester.Id
+        },
+        new()
+        {
+            Id = 201,
+            Name = "Psychology"
+        }
+    };
+
+    [Test]
+    public void EmptyFilter_ReturnsEntireQuery()
+    {
+        // Arrange
+        CoursesFilterProperties properties = new();
+        CoursesFilter filter = new(properties, string.Empty);
+
+        // Act
+        var result = filter.Apply(TestCourses.AsQueryable());
+
+        // Assert
+        Assert.That(result, Is.EquivalentTo(TestCourses));
+    }
+
+    [Test]
+    public void SemesterIdSpecified_ReturnsFilteredQuery()
+    {
+        // Arrange
+        CoursesFilterProperties properties = new(SemesterId: TestSemester.Id);
+        CoursesFilter filter = new(properties, string.Empty);
+        int[] expectedIds = { 200 };
+
+        // Act
+        var actualIds = filter.Apply(TestCourses.AsQueryable())
+            .Select(g => g.Id);
+
+        // Assert
+        Assert.That(actualIds, Is.EquivalentTo(expectedIds));
+    }
+
+    [Test]
+    public void ZeroSemesterId_ReturnsGroupsWithoutSemesters()
+    {
+        // Arrange
+        CoursesFilterProperties properties = new(SemesterId: 0);
+        CoursesFilter filter = new(properties, string.Empty);
+        int[] expectedIds = { 201 };
+
+        // Act
+        var actualIds = filter.Apply(TestCourses.AsQueryable())
+            .Select(g => g.Id);
+
+        // Assert
+        Assert.That(actualIds, Is.EquivalentTo(expectedIds));
+    }
+}
---
+++ Infrastructure/Filters/CoursesFilter.cs
@@ -0,0 +1,34 @@
+﻿using EUniversity.Core.Models.University;
+
+namespace EUniversity.Infrastructure.Filters;
+
+/// <summary>
+/// Filter for classes.
+/// </summary>
+public class CoursesFilter : DefaultFilter<Course>
+{
+    /// <summary>
+    /// Properties of filtering.
+    /// </summary>
+    public CoursesFilterProperties Properties { get; }
+
+    public CoursesFilter(CoursesFilterProperties properties, string name,
+        DefaultFilterSortingMode sortingMode = DefaultFilterSortingMode.Default) :
+        base(name, sortingMode)
+    {
+        Properties = properties;
+    }
+
+    /// <inheritdoc />
+    public override IQueryable<Course> Apply(IQueryable<Course> query)
+    {
+        // Apply courses filter
+        if (Properties.SemesterId != null)
+        {
+            int? semesterId = Properties.SemesterId != 0 ? Properties.SemesterId : null;
+            query = query.Where(c => c.SemesterId == semesterId);
+        }
+        // Apply default filter
+        return base.Apply(query);
+    }
+}
