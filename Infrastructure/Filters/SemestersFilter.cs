cf5ace229f39e71a847f4ec752a4028aef7e84da
romandykyi <roman.dykyi.8210@gmail.com>
2023-12-16 15:24:05 +0100

    Implement `SemestersFilter`

---
+++ Infrastructure/Filters/SemestersFilter.cs
@@ -0,0 +1,37 @@
+﻿using EUniversity.Core.Models.University;
+
+namespace EUniversity.Infrastructure.Filters;
+
+/// <summary>
+/// Filter for classes.
+/// </summary>
+public class SemestersFilter : DefaultFilter<Semester>
+{
+    /// <summary>
+    /// Properties of filtering.
+    /// </summary>
+    public SemestersFilterProperties Properties { get; }
+
+    public SemestersFilter(SemestersFilterProperties properties, string name,
+        DefaultFilterSortingMode sortingMode = DefaultFilterSortingMode.Default) :
+        base(name, sortingMode)
+    {
+        Properties = properties;
+    }
+
+    /// <inheritdoc />
+    public override IQueryable<Semester> Apply(IQueryable<Semester> query)
+    {
+        // Apply semesters filter
+        if (Properties.MaxDateFrom != null)
+        {
+            query = query.Where(s => s.DateFrom <= Properties.MaxDateFrom);
+        }
+        if (Properties.MinDateTo != null)
+        {
+            query = query.Where(s => s.DateTo >= Properties.MinDateTo);
+        }
+        // Apply default filter
+        return base.Apply(query);
+    }
+}
