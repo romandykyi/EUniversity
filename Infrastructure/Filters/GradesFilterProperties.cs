7f213631f97b27fa0b3b1b0ae0ecb20be08b360c
romandykyi <roman.dykyi.8210@gmail.com>
2023-12-17 23:04:03 +0100

    Apply grades filter to the endpoint

--- Infrastructure/Filters/GradesFilterProperties.cs
+++ Infrastructure/Filters/GradesFilterProperties.cs
@@ -5,4 +5,4 @@
 /// </summary>
 /// <param name="Name">Optional name to filter by.</param>
 /// <param name="SortingMode">Sorting mode for grades.</param>
-public record GradesFilterProperties(string Name = "", GradesSortingMode SortingMode = GradesSortingMode.Default);
+public record GradesFilterProperties(string Name = "", GradesSortingMode SortingMode = GradesSortingMode.ScoreDescending);
