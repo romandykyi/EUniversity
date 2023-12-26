3b230b2b9d9dfc69a1a5c885fb68e18f8ae80e10
romandykyi <roman.dykyi.8210@gmail.com>
2023-12-26 20:59:25 +0100

    Fix dto property name

--- Core/Dtos/University/Grades/AssignedGradeViewDto.cs
+++ Core/Dtos/University/Grades/AssignedGradeViewDto.cs
@@ -5,6 +5,6 @@ namespace EUniversity.Core.Dtos.University.Grades;
 public record AssignedGradeViewDto(int Id, string? Notes, 
     GradeMinimalViewDto Grade, GroupMinimalViewDto? Group,
     UserViewDto? Assigner, UserViewDto? Reassigner,
-    StudentPreviewDto? StudentPreview,
+    StudentPreviewDto? Student,
     DateTimeOffset CreationDate, DateTimeOffset UpdateDate,
     ActivityTypeMinimalDto? ActivityType);
